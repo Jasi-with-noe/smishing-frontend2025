addc6a26c8552ed22f2a6f0358ca8cd5fee1c2aa
Jas-with-noe <jasmin.smallu2001@gmail.com>
2025-04-21 15:56:12 +1000

    Restore OCR drawable assets and okhttp dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -118,5 +118,6 @@ dependencies {
     implementation("com.google.android.gms:play-services-auth:20.0.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation ("com.tbuonomo:dotsindicator:4.3")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.3")
 }
 
